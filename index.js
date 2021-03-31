d7ac844ec10fd510a58a4f252f4cf7b348f22e69
Corrinne W <corrinneworden@gmail.com>
2021-03-31 16:43:28 -0400

    working on addEmployee funciton...debugging prompt response

--- index.js
+++ index.js
@@ -32,10 +32,30 @@ const questions = [
 
 //builds HTML from data received
 function buildHTML() {
+    console.log(myTeam)
 };
 
 //allows user to add as many engineers and interns as they like
-function addEmployee() {};
+function addEmployee() {
+    inquirer.prompt ({
+        type: 'checkbox',
+        name: 'addEmployee',
+        message: 'Would you like to add a new team member?',
+        choices: ['Add a new Engineer', 'Add a new Intern', "I'm done. Finish building my team."]
+    })
+    .then(response => {
+        console.log(response)
+        // if (response === ['Add a new Engineer']) {
+        //     newEngineer();
+        // } else if (response === ['Add a new Intern']) {
+        //     newIntern();
+        // } else if (response === ["I'm done. Finish building my team."]) {
+        //     buildHTML();
+        // } else {
+        //     addEmployee();
+        // }
+    })
+};
 
 //function to fill in engineer info
 function newEngineer() {
@@ -48,14 +68,16 @@ function newEngineer() {
     //adds EngineerQuestion to the questions array before it is prompted by inquirer
     questions.push(engineerQuestion);
 
+    console.log("Please enter the engineer's information:")
+
     inquirer
         .prompt(questions)
         .then(data => {
             const newEngineer = new Engineer(data.firstLastName, data.id, data.email, data.github);
             myTeam.push(newEngineer);
-        });
 
-    addEmployee();
+            addEmployee();
+        });
 };
 
 //function to fill in intern info
@@ -69,14 +91,16 @@ function newIntern() {
     //adds internQuestion to the questions array before it is prompted by inquirer
     questions.push(internQuestion);
 
+    console.log("Please enter the intern's information:")
+
     inquirer
         .prompt(questions)
         .then(data => {
             const newIntern = new Intern(data.firstLastName, data.id, data.email, data.school);
             myTeam.push(newIntern);
+
+            addEmployee();
         });
-    
-    addEmployee();
 };
 
 //add a team manager
@@ -95,13 +119,14 @@ function newManager() {
         .then(data => {
             const newManager = new Manager(data.firstLastName, data.id, data.email, data.officeNumber);
             myTeam.push(newManager);
+
+            addEmployee();
         });
-    
-    addEmployee();
 };
 
 
 function init() {
+    console.log("Welcome to the Team Profile Generator! Start by entering your team manager's information:")
     newManager();
 };
 
